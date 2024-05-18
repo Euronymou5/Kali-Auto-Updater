8d6504588b3a5b38ff759cd8791cf47d01fd3cbc
Euronymou5 <85043356+Euronymou5@users.noreply.github.com>
2024-05-17 19:31:37 -0600

    Actualizacion y solucion de error: : Problem executing scripts APT::Update::Post-Invoke-Success

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 def menu():
    os.system("clear")
@@ -11,11 +12,14 @@ def menu():
           
    ''')
    
+   print('\n\033[34m[~] Descargando clave publica...')
    os.system('wget "archive.kali.org/archive-key.asc"')
    os.system("apt-key add archive-key.asc")
+   time.sleep(2)
    
    print("\n\033[32m[✓] Clave publica actualizada.")
-   print("\033[34m[~] Modificando archivo: /etc/apt/sources.list")
+   print("\n\033[34m[~] Modificando archivo: /etc/apt/sources.list")
+   time.sleep(2)
    
    os.system("rm /etc/apt/sources.list")
    with open("sources.list", 'w') as f:
@@ -23,13 +27,28 @@ def menu():
 deb https://http.kali.org/kali kali-rolling main contrib non-free non-free-firmware
 
 # Additional line for source packages
-# deb-src http://http.kali.org/kali kali-rolling main contrib non-free
-
-       """)
+# deb-src http://http.kali.org/kali kali-rolling main contrib non-free""")
    os.system("mv sources.list /etc/apt")
    
    print("\033[32m\n[✓] Archivo movido exitosamente.")
-   print("\033[32m\n[✓] Actualizacion completada.")
+   time.sleep(2)
+   
+   print('\n\033[34m[~] Solucionando errores...')
+   
+   print('\n[~] Reinstalando command-not-found...')
+   time.sleep(2)
+   os.system("sudo apt install --reinstall command-not-found")
+   
+   print('\n[~] Configurando los paquetes pendientes...')
+   time.sleep(2)
+   os.system("sudo dpkg --configure -a")
+   
+   print('\n[~] Eliminando paquetes innecesarios...')
+   time.sleep(2)
+   os.system("sudo apt autoremove")
+   
+   time.sleep(2)
+   print("\n\033[32m\n[✓] Actualizacion completada.")
    
 if __name__ == '__main__':
     menu()
